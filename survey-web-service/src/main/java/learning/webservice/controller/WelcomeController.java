124474b3cae38344d7e0e088f25ae015ac586695
Sundar Annamalai <sundarannamalai@gmail.com>
2018-05-01 11:24:16 +0530

    spring dynamic configuration

--- survey-web-service/src/main/java/learning/webservice/controller/WelcomeController.java
+++ survey-web-service/src/main/java/learning/webservice/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package learning.webservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+  @Value("${welcome.message}")
+  private String message;
+
   @GetMapping("/welcome")
   public String welcome() {
-    return "Welcome!";
+    return message;
   }
 }
