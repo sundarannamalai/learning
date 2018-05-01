e5fc5db5e6e9c5719d34a0373fecae14b0e08dbd
Sundar Annamalai <sundarannamalai@gmail.com>
2018-05-01 11:38:14 +0530

    spring profiles

--- survey-web-service/src/main/java/learning/webservice/controller/WelcomeController.java
+++ survey-web-service/src/main/java/learning/webservice/controller/WelcomeController.java
@@ -1,6 +1,8 @@
 package learning.webservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class WelcomeController {
   @GetMapping("/welcome")
   public String welcome() {
     return message;
+  }
+
+  @Profile("dev")
+  @Bean
+  private String dummy() {
+    return "something else";
   }
 }
