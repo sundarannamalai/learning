f358ef8d210a2a4ace6154b06a98bf9bab7857ba
Sundar Annamalai <sundarannamalai@gmail.com>
2019-08-23 14:35:24 -0400

    spring batch and rabbitmq changes

---
+++ demo-rabbitmq/src/main/java/com/learning/rabbitmq/SimpleMessage.java
@@ -0,0 +1,11 @@
+package com.learning.rabbitmq;
+
+/**
+ * Created by
+ * <p>
+ * User: sannamalai
+ * Date: 15/09/18
+ * Time: 7:37 PM
+ */
+public class SimpleMessage {
+}
---
+++ demo-spring-batch/src/main/java/learning/spring/batch/BatchSampleApplication.java
@@ -0,0 +1,12 @@
+package learning.spring.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BatchSampleApplication.class, args);
+  }
+}
